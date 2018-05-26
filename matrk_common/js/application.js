62762e86832fb7ffc751b0986a3e73dbd776d1c4
Akihiro Sada <sada@netlab.jp>
2018-05-26 15:56:31 +0900

    640f80d 時点のファイルを生成。

---
+++ matrk_common/js/application.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  //ページ内スクロール
+  $(".menu a").click(function () {
+    var mark = $(".mark").offset().top;
+    var id = $(this).attr("href");
+    var point = 120;
+    try {
+      point = $(id).offset().top;
+    } catch(e) {
+      // $('#') は例外になるため無視。
+    }
+    $('html,body').animate({ scrollTop: point - 120 }, 'fast');
+    return false;
+  });
+
+});
